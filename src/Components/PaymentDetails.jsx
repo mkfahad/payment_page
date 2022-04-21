b9d8cecec3f6c022972bc6d8f1ef042a8e8a4a7b
Fahad <fahad@Fahads-MacBook-Air.local>
2022-04-21 11:39:11 +0530

    Minor bugs fixed

--- src/Components/PaymentDetails.jsx
+++ src/Components/PaymentDetails.jsx
@@ -5,9 +5,8 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import PaymentCardVisa from './PaymentCardVisa';
+import PaymentCard from './PaymentCardVisa';
 import PaymentCardMaster from './PaymentCardMaster';
-import checkicon from '../assets/check_circle.svg'
 function PaymentDetails() {
     const [open, setOpen] = React.useState(false);
 
@@ -51,7 +50,18 @@ function PaymentDetails() {
                                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
                                             Expiry Date
                                         </label>
-                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="07/22" />
+                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="MM" />
+                                    </div>
+                                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                        <div class="relative">
+                                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                                Expiry Date
+                                            </label>
+                                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="YY" />
+                                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
@@ -65,7 +75,7 @@ function PaymentDetails() {
                                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                                             First Name
                                         </label>
-                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+                                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
                                     </div>
                                     <div class="w-full md:w-1/2 px-3">
                                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
@@ -73,7 +83,6 @@ function PaymentDetails() {
                                         </label>
                                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
                                     </div>
-                                    <p className='flex text-xs text-black'><img src={checkicon}/>Make this my Default Credit Card</p>
                                 </div>
                             </form>
 
@@ -87,7 +96,7 @@ function PaymentDetails() {
             </div>
             <div className='h-[1px] w-full bg-gray-200' />
             <div>
-                <PaymentCardVisa />
+                <PaymentCard/>
             </div>
             <div className='text-gray-600 flex'>
                 <h3>Other Payment Method</h3>
@@ -95,7 +104,7 @@ function PaymentDetails() {
             <div className='h-[1px] w-full bg-gray-200' />
             <div className='md:flex space-x-2 grid-col-1 pb-3 space-y-3'>
                 <PaymentCardMaster />
-                <PaymentCardVisa />
+                <PaymentCard />
             </div>
         </div>
     )
